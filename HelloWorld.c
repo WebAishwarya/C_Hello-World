fa72270abf2c3d1d6848650067b9155c6f584e3b
WebAishwarya <140585542+WebAishwarya@users.noreply.github.com>
2024-10-18 22:44:22 +0530

    HelloWorld.c

---
+++ HelloWorld.c
@@ -0,0 +1,48 @@
+#include<stdio.h>
+
+/*
+    #include - Preprocessor Directive or command
+    #define, #if, #endif ...
+    
+    <stdio.h>
+    <> - Angular Bracket
+    {} - Block of code or statements
+    [] - Arrays
+    () - Function
+    
+    stdio - Standard Input Output
+    .h - Header File
+    
+    .(extension)
+    [   .png,
+        .jpg,
+        .mp4,
+        .mp3,
+        .pdf,
+        .txt,
+        .doc,
+        .bin ]
+    [   .c,
+        .cpp,
+        .java,
+        .py,
+        .html,
+        .php...]
+    
+    void main()
+    void - Keyword (nothing)
+    
+    main() - Entry point of execution of any program
+*/
+
+void main()
+{  //Start of Block
+    
+    printf("Hello World !");
+    /*
+        " " - String , ' ' - Character
+        Displaying the output on the console
+        ; - End of statements
+    */
+    
+}   //End of Block
